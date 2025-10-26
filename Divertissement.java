acdd01700119560d5c33f82f34cbd811f9d9d237
Hazem Barkallah <barkhazem@gmail.com>
2025-10-27 00:44:42 +0100

    Add files via upload

---
+++ Divertissement.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hazem.activite;
+
+/**
+ *
+ * @author HP
+ */
+public final class Divertissement extends Activite {
+    private String CategorieAge;
+    public Divertissement(String nom, String type, int duree, String CategorieAge) {
+        super(nom, type, duree);
+        this.CategorieAge=CategorieAge;
+    }
+}
