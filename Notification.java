16d7adfc9962771a5fdb2d89ba08f10026b48b54
Hazem Barkallah <barkhazem@gmail.com>
2025-10-27 00:51:25 +0100

    Refactor Notification class to new package

--- Notification.java
+++ Notification.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.hazem.notification;
+package smarttimeapp;
 
 /**
  *
@@ -36,4 +36,5 @@ public class Notification {
             + "\nLue: " + (lue ? "Oui" : "Non");
     }
 }
+
 
