81ffda9e72f55da2460154366395e8abb1668b24
Hazem Barkallah <barkhazem@gmail.com>
2025-10-27 00:50:35 +0100

    Refactor Divertissement class package and comments

--- Divertissement.java
+++ Divertissement.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.hazem.activite;
+package smarttimeapp;
 
 /**
  *
@@ -15,3 +15,4 @@ public final class Divertissement extends Activite {
         this.CategorieAge=CategorieAge;
     }
 }
+
