9ac25c735ecdb96dc51bc775f9f93c9eba9e3fdd
Hazem Barkallah <barkhazem@gmail.com>
2025-10-27 00:48:17 +0100

    Refactor Utilisateur class and update package name

--- Utilisateur.java
+++ Utilisateur.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.hazem.utilisateur;
+package smarttimeapp;
 import java.util.List;
 import java.util.ArrayList;
 /**
@@ -38,3 +38,4 @@ public sealed abstract class Utilisateur permits Adulte, Enfant{
         return result;
     }
 }
+
